539d34412bdfebeeb8ee8bc317e4bc8ee1a45fe4
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2022-10-16 22:51:02 +0800

    Add files via upload

---
+++ data_loader.py
@@ -0,0 +1,62 @@
+import torch
+import torchvision.datasets as dsets
+from torchvision import transforms
+
+
+class Data_Loader():
+    def __init__(self, train, dataset, image_path, image_size, batch_size, shuf=True):
+        self.dataset = dataset
+        self.path = image_path
+        self.imsize = image_size
+        self.batch = batch_size
+        self.shuf = shuf
+        self.train = train
+
+    def transform(self, resize, totensor, normalize, centercrop):
+        options = []
+        if centercrop:
+            options.append(transforms.CenterCrop(160))
+        if resize:
+            options.append(transforms.Resize((self.imsize,self.imsize)))
+        if totensor:
+            options.append(transforms.ToTensor())
+        if normalize:
+            options.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
+        transform = transforms.Compose(options)
+        return transform
+
+    def load_lsun(self, classes='church_outdoor_train'):
+        transforms = self.transform(True, True, True, False)
+        dataset = dsets.LSUN(self.path, classes=[classes], transform=transforms)
+        return dataset
+
+    def load_celeb(self):
+        transforms = self.transform(True, True, True, True)
+        dataset = dsets.ImageFolder(self.path+'/CelebA', transform=transforms)
+        return dataset
+
+
+    def loader(self):
+        if self.dataset == 'lsun':
+            dataset = self.load_lsun()
+        elif self.dataset == 'celeb':
+            dataset = self.load_celeb()
+        else:
+            import torchvision
+            dataset = torchvision.datasets.ImageFolder(self.dataset,
+                                                        transform=transforms.Compose([
+                                                        transforms.Resize(self.imsize),
+                                                        #transforms.RandomHorizontalFlip(),
+                                                        #transforms.Scale(64),
+                                                        transforms.CenterCrop(self.imsize),                                                 
+                                                        transforms.ToTensor(),
+                                                        #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #GANomaly parameter
+                                                        ])
+                                                        )
+        loader = torch.utils.data.DataLoader(dataset=dataset,
+                                              batch_size=self.batch,
+                                              shuffle=self.shuf,
+                                              num_workers=2,
+                                              drop_last=True)
+        return loader
+
